aa4bc1ddcdfa52fa3ef0929f88b4da75688669a8
Maninder Singh <maninderrai7@gmail.com>
2017-03-20 22:28:31 +0530

    Fix get_song_artist_id
    - If song_id and artist_id does not exists insert them

--- models/SongRankings.py
+++ models/SongRankings.py
@@ -1,3 +1,8 @@
+from models.Artist import Artist
+from models.Song import Song
+from models.SongArtist import SongArtist
+
+
 class SongRankings():
     def __init__(self, song, artist, source, rank, week):
         self.song = song
@@ -5,8 +10,6 @@ class SongRankings():
         self.source = source
         self.rank = rank
         self.week = week
-        self.song_id = None
-        self.artist_id = None
 
     def _absorb_db_row(self, row, cursor):
         self.song_id = row[1]
@@ -21,9 +24,9 @@ class SongRankings():
         """
         duplicate_row = cursor.execute(
             """
-            SELECT * from song_rankings WHERE week = ?;
+            SELECT * from song_rankings WHERE rank = ? AND week = ?;
             """,
-            (self.week,)
+            (self.rank, self.week)
         ).fetchone()
 
         if duplicate_row:
@@ -31,18 +34,23 @@ class SongRankings():
 
         return duplicate_row
 
-    def select(self, cursor):
+    def get_song_artist_id(self, cursor):
         """
         Select song_id and artist_id from database
         """
         try:
-            self.song_id, self.artist_id = cursor.execute(
+            song_artist_id = cursor.execute(
                 """
-                SELECT song.id AS song_id, artist.id AS artist_id
-                where song.name = ? and artist.name = ?
+                SELECT song.id AS "song_id", artist.id AS "artist_id"
+                FROM song_artist
+                INNER JOIN song ON song_artist.song_id = song.id
+                INNER JOIN artist ON song_artist.artist_id = artist.id
+                WHERE song.name=? and artist.name=?;
                 """,
                 (self.song, self.artist)
-            )
+            ).fetchone()
+
+            return song_artist_id
         except Exception as error:
             print("Error while selecting song_id and artist_id: ", error)
             raise error
@@ -51,6 +59,21 @@ class SongRankings():
         """
         Insert song_rankings to database. Fail if song rankings_already exits
         """
+        song_artist_id = self.get_song_artist_id(cursor)
+
+        if not song_artist_id:
+            artist_id = Artist(self.artist).insert(cursor).id
+            song_id = Song(self.song,
+                           None,
+                           None,
+                           None,
+                           artist_id).insert(cursor).id
+            SongArtist(song_id,
+                       artist_id).insert(cursor)
+        else:
+            song_id = song_artist_id[0]
+            artist_id = song_artist_id[1]
+
         if self.check_duplicate(cursor):
             return self
 
@@ -62,8 +85,12 @@ class SongRankings():
                 VALUES
                 (?, ?, ?, ?, ?);
                 """,
-                (self.song_id, self.artist_id, self.source, self.rank, self.week).lastrowid
-            )
+                (song_id,
+                 artist_id,
+                 self.source,
+                 self.rank,
+                 self.week)
+            ).lastrowid
         except Exception as error:
             print("Error while inserting song_rankings: ", error)
             raise error
