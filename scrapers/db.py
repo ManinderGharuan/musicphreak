88c0362edbaba71f0206d6514455704e6c8b8017
Maninder Singh <maninderrai7@gmail.com>
2017-04-09 09:22:23 +0530

    Remove app.config

--- scrapers/db.py
+++ scrapers/db.py
@@ -1,30 +1,33 @@
 import sqlite3
 from os import path
 
+config = {
+    'SCRAPER_DB': path.join(path.dirname(__file__), 'scraper.db')
+}
 
-def connect_db(app):
+
+def connect_db():
     """
     Connects to the scraper database.
     """
-    rv = sqlite3.connect(app.config['SCRAPER_DB'])
+    rv = sqlite3.connect(config['SCRAPER_DB'])
     rv.row_factory = sqlite3.Row
 
     return rv
 
 
-def get_db(app):
+def get_db():
     """
-    Opens a new database connection if there is none yet for the
-    current application context.
+    Opens and returns a new database connection
     """
-    if not path.exists(app.config['SCRAPER_DB']):
-        init_db(app)
+    if not path.exists(config['SCRAPER_DB']):
+        init_db()
 
-    return connect_db(app)
+    return connect_db()
 
 
-def init_db(app):
-    db = connect_db(app)
+def init_db():
+    db = connect_db()
     print("CREATING DATABASE")
 
     with open('scrapers/schema.sql', mode='r') as f:
